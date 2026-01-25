c63f91badde83d6b4cbf359f879b71eb6a8f8b2c
Vishal Sigdel <sigdelvishal123@gmail.com>
2026-01-25 15:19:47 +0545

    FIX: removed commits

--- facet/app/dashboard/layout.tsx
+++ facet/app/dashboard/layout.tsx
@@ -15,7 +15,6 @@ export default async function DashboardLayout({
 
   return (
     <div className="flex flex-1 overflow-hidden h-[calc(100vh-3.5rem)]">
-        {/* We let the page render the sidebar because it needs data fetching logic tied to state */}
         {children}
     </div>
   );
